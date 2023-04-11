4ee69a51185ef0ed79f55255c3fcfa7890f14675
Serg <ksyssau@ya.ru>
2023-04-11 08:46:52 +0400

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,28 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-    int [] arr1 = new int[]{1 , 2 , 3};
-    float [] arr2 = {1.57f, 7.654f, 9.986f};
-    int [] arr3 = {16, 18, 20};
+        int[] arr1 = new int[]{1, 2, 3};
+        float[] arr2 = {1.57f, 7.654f, 9.986f};
+        int[] arr3 = {16, 18, 20};
+        for (int i = 0; i < 3; i++) {   //печатаем первый массив
+            System.out.print(arr1[i]);
+            if (i != 2) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < 3; i++) { //печатаем второй массив
+            System.out.print(arr2[i]);
+            if (i != 2) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < 3; i++) { //печатаем третий массив
+            System.out.print(arr3[i]);
+            if (i != 2) {
+                System.out.print(",");
+            }
+        }
     }
 }
