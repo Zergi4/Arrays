55c1f7e593a027acc86eb5ea8231990f893d1e5f
Serg <ksyssau@ya.ru>
2023-04-11 08:37:15 +0400

    1 задача

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+    int [] arr1 = new int[]{1 , 2 , 3};
+    float [] arr2 = {1.57f, 7.654f, 9.986f};
+    int [] arr3 = {16, 18, 20};
     }
 }
