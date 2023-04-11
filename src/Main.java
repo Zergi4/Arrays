0d2929188e2921256618a1bb9600c2f9f5bf52a5
Serg <ksyssau@ya.ru>
2023-04-11 09:02:06 +0400

    4 задача

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -46,7 +48,13 @@ public class Main {
                 System.out.print(",");
             }
         }
-
+        System.out.println();
+        for (int i = 0; i < 3; i++) { //четвертая задача
+            if ((arr1[i] % 2) != 0) {
+                arr1[i]++;
+            }
+        }
+        System.out.println(Arrays.toString(arr1));
 
     }
 }
